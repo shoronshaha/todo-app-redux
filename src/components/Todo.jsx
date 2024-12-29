9d8ea93b83f20d0486b040f77506028b6d48773a
Smaran Shaha <smaranshaha@gmail.com>
2024-12-29 20:47:09 +0600

    added deleted button for user ui and work it

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import { Check, X } from "lucide-react";
 import { useDispatch } from "react-redux";
-import { toggled } from "../redux/todos/actions";
+import { toggled, colorSelected, deleted } from "../redux/todos/actions";
 
 const Todo = ({ todo }) => {
   const dispatch = useDispatch();
@@ -11,8 +11,13 @@ const Todo = ({ todo }) => {
     dispatch(toggled(todoId));
   };
 
-  console.log("completed :>> ", completed);
+  const handleColorChange = (todoId, color) => {
+    dispatch(colorSelected(todoId, color));
+  };
 
+  const handleDelete = (todoId) => {
+    dispatch(deleted(todoId));
+  };
   return (
     <>
       <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
@@ -36,19 +41,22 @@ const Todo = ({ todo }) => {
           className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-green-500 border-green-500 ${
             color === "green" && "  hover:bg-green-500 bg-green-500"
           }`}
+          onClick={() => handleColorChange(id, "green")}
         ></div>
         <div
           className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-yellow-500 border-yellow-500 ${
             color === "yellow" && " border-yellow-500 bg-yellow-500"
           }`}
+          onClick={() => handleColorChange(id, "yellow")}
         ></div>
         <div
           className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer hover:bg-red-500 border-red-500 ${
             color === "red" && " border-red-500 bg-red-500"
           }`}
+          onClick={() => handleColorChange(id, "red")}
         ></div>
 
-        <X />
+        <X onClick={() => handleDelete(id)} />
       </div>
     </>
   );
